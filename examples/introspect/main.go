1609f41dd75db056cd6503266a57e69618f82acb
Martin Wimpress <martin@wimpress.org>
2025-11-14 10:50:40 +0000

    feat(ffmpeg): add bitstream filter support
    - Implement wrapper for AVBitStreamFilter struct with methods for accessing name, codec IDs, and private class
    - Add AVBSFIterate function to iterate over all registered bitstream filters
    - Include necessary header (libavcodec/bsf.h) and external declaration
    - Add listBSFs function to the introspect example to demonstrate the new functionality

--- examples/introspect/main.go
+++ examples/introspect/main.go
@@ -35,6 +35,7 @@ func main() {
 	listHWAccels()
 	listFormats()
 	listFilters()
+	listBSFs()
 	listParsers()
 	listProtocols()
 }
@@ -393,6 +394,63 @@ func listFormats() {
 	fmt.Printf("  Total formats: %d\n", len(formats))
 	fmt.Printf("  Total demuxers: %d\n", totalDemuxers)
 	fmt.Printf("  Total muxers: %d\n", totalMuxers)
+}
+
+func listBSFs() {
+	fmt.Println("\n==================================================")
+	fmt.Println("BITSTREAM FILTERS")
+	fmt.Println("==================================================\n")
+	fmt.Printf("    %-24s %-42s\n", "NAME", "SUPPORTED CODECS")
+	fmt.Println()
+
+	var opaque unsafe.Pointer
+	count := 0
+	codecSpecificCount := 0
+
+	for {
+		bsf := ffmpeg.AVBSFIterate(&opaque)
+		if bsf == nil {
+			break
+		}
+		count++
+
+		// Get the bitstream filter name
+		name := bsf.Name()
+
+		// Truncate name if too long
+		if len(name) > 24 {
+			name = name[:24]
+		}
+
+		// Get supported codec IDs
+		codecList := "all"
+		codecIDs := bsf.CodecIds()
+		if codecIDs != nil {
+			codecSpecificCount++
+			var codecs []string
+			for i := uintptr(0); ; i++ {
+				codecID := (*ffmpeg.AVCodecID)(unsafe.Pointer(uintptr(unsafe.Pointer(codecIDs)) + i*unsafe.Sizeof(*codecIDs)))
+				if *codecID == ffmpeg.AVCodecIdNone {
+					break
+				}
+				codecName := getCodecName(*codecID)
+				codecs = append(codecs, codecName)
+			}
+			if len(codecs) > 0 {
+				codecList = strings.Join(codecs, ", ")
+				if len(codecList) > 64 {
+					codecList = codecList[:61] + "..."
+				}
+			}
+		}
+
+		fmt.Printf("    %-24s %-42s\n", name, codecList)
+	}
+
+	fmt.Printf("\nSummary:\n")
+	fmt.Printf("  Total bitstream filters: %d\n", count)
+	fmt.Printf("  Codec-specific filters: %d\n", codecSpecificCount)
+	fmt.Printf("  Generic filters: %d\n", count-codecSpecificCount)
 }
 
 func listParsers() {
